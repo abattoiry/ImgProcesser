54b8c1147d8f01fc8bd14d688d3e501c15850f7b
Y WANG <wangyue08507@hellobike.com>
2019-06-03 18:07:34 +0800

    improve

--- src/img-compare.js
+++ src/img-compare.js
@@ -1,22 +1,15 @@
+/* eslint-disable */
 const { compare } = require('resemblejs');
 const fs = require('fs');
 const glob = require('glob');
 
+// 超过此值不在继续计算区别，提高效率
+const compareThreshold = 5;
+
 const options = {
-  output: {
-    errorColor: {
-      red: 255,
-      green: 0,
-      blue: 255
-    },
-    errorType: 'movement',
-    transparency: 0.3,
-    largeImageThreshold: 1200,
-    useCrossOrigin: true,
-    outputDiff: false,
-  },
   scaleToSameSize: false,
-  ignore: null
+  ignore: null,
+  returnEarlyThreshold: compareThreshold,
 };
 
 function imgCompare(img1, img2) {
@@ -29,20 +22,20 @@ function imgCompare(img1, img2) {
           console.log('resemble compare error', err);
           reject(err);
         } else {
-          resolve(data)
+          resolve(data);
         }
       }
-    );
-  })
+    )
+  });
 }
 
-// 总列表
+// 所有相似图片组
 const imgArrs = [];
 (async function run() {
   let files;
   try {
     files = await new Promise((resolve, reject) => {
-      glob('**/*.png', (err, data) => {
+      glob('./src/**/*.png', (err, data) => {
         if (err) {
           reject(err);
         }
@@ -56,24 +49,25 @@ const imgArrs = [];
   let item, flag;
   do {
     flag = true;
-    // 子列表
-    const arr = [];
-    // 每次取出来第一个
+    const arr = []; // 一组相似图片
+    // 每次取出来一张图片
     item = files.shift();
-    // 从第二个开始遍历
+    // 遍历剩下的图片列表
     for (let i = 0; i < files.length; i++) {
       const _item = files[i];
       try {
-        let res = await imgCompare(item, _item)
-        if (res.rawMisMatchPercentage < 20) {
-          // 如果是这次do的第一次匹配就将取出来的item push进列表，并将列表push到总列表里
+        let res = await imgCompare(item, _item);
+        if (res.rawMisMatchPercentage < compareThreshold) {
+          // 每次遍历对比的第一次成功
           if (flag) {
+            // 将之前取出的图片放入输出列表里，证明这张图片是有类似图片的
             arr.push(item);
             imgArrs.push(arr);
             flag = false;
           }
+          // 将对比成功的图片放入输出列表
           arr.push(_item);
-          // 从列表中去除掉
+          // 从文件列表中去除掉对比成功的图片，以后不再作对比
           files.splice(i, 1);
           // 列表缩短了所以index要-1
           i--;
@@ -82,10 +76,14 @@ const imgArrs = [];
         console.log('resemble compare error', err);
       }
     }
+    if (arr.length > 0) {
+      console.log(arr);
+      console.log('========================================')
+    }
   } while (item);
-  console.log(imgArrs);
+  console.log(`一共存在${imgArrs.length}组相似图片`);
 })()
 
 module.exports = {
   imgCompare
-}
+};
