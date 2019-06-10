9a39db57e0d0af731f6267468bcbc234f1ebaf5a
Y WANG <wangyue08507@hellobike.com>
2019-06-10 19:47:37 +0800

    optimize

--- src/utils/scan-image-refs.js
+++ src/utils/scan-image-refs.js
@@ -15,7 +15,7 @@ async function scanImageRefs(callback) {
       let imgPath = result[3];
       const originalImgPath = result[3];
       if (imgPath.startsWith('/')) {
-        imgPath = path.resolve(`.${imgPath}`);
+        imgPath = path.resolve(config.imgAbsoluteRoot, `.${imgPath}`);
       }
       switch (imgPath.slice(0, 2)) {
         case '@/':
